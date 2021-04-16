61a23d2194c5f57ac3ad76a596fecfb736f18153
Clyde Brown <clydiscope@gmail.com>
2021-04-16 19:53:56 -0400

    adds campsite info to single page app

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -23,17 +23,30 @@ class Main extends Component {
       };
   }
 
+  
+
   render() {
 
       const HomePage = () => {
           return (
               <Home 
-              campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
                     promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
                     partner={this.state.partners.filter(partner => partner.featured)[0]}
               />
           );
       };
+
+      const CampsiteWithId = ({match}) => {
+        return (
+            <CampsiteInfo 
+                campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+            />
+        );
+      };    
+    
+    
 
       return (
           <div>
@@ -41,6 +54,7 @@ class Main extends Component {
               <Switch>
                   <Route path='/home' component={HomePage} />
                   <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                  <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                   <Route exact path='/contactus' component = {Contact}/>
                   <Redirect to='/home' />
               </Switch>
