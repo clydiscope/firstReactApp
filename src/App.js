5b1e2b2c815844c7f96cafd194b281029607267f
Clyde Brown <clydiscope@gmail.com>
2021-04-16 16:35:21 -0400

    adds header and footer components and router features

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
-import Directory from './components/DirectoryComponents';
+import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/"> NuCamp </NavbarBrand>{" "}
-          </div>{" "}
-        </Navbar>{" "}
-        <Directory />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>  
     );
   }
 }
