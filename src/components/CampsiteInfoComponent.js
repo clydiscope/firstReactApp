020572967bb8726d86aea16e3a44fae041b81ed4
Clyde Brown <clydiscope@gmail.com>
2021-05-01 00:13:00 -0400

    adds redux-thunk and logger to handle asynchronous calls, and a loading icon

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Button, Breadcrumb, BreadcrumbItem, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import {LocalForm, Control, Errors} from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -122,6 +123,28 @@ function RenderComments({comments, addComment, campsiteId}) {
     
 
 function CampsiteInfo(props){
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        )
+    }
+
+    if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     if(props.campsite){
         return(
         <div className="container">
---
+++ src/components/LoadingComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Loading = () => {
+    return (
+        <div className="col">
+            <i className="fa fa-spinner fa-pulse fa-3x ra-fw text-primary" />
+            <p>Loading...</p>
+        </div>
+    );
+};
