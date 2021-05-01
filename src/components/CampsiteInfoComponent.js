663550733a54c81393db675bf78ec8297106460b
Clyde Brown <clydiscope@gmail.com>
2021-04-30 22:48:32 -0400

    adds add_comment action and reducer

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -48,8 +48,8 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         console.log(values)
-        alert(`Rating: ${values.rating} Author: ${values.author} Text: ${values.text}`);
         this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text)
     }
 
 
@@ -97,7 +97,7 @@ class CommentForm extends Component {
     }
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, campsiteId}) {
     if(comments) {
         return(
             <div className="col-md-5 m-1">
@@ -112,7 +112,7 @@ function RenderComments({comments}) {
                     );
                     })
                 }
-                <CommentForm/>
+                <CommentForm campsiteId={campsiteId} addComment={addComment}/>
 
             </div>
         );
@@ -137,7 +137,9 @@ function CampsiteInfo(props){
             </div>            
             <div className="row"> 
                 <RenderCampsite campsite={props.campsite} /> 
-                <RenderComments comments={props.comments} /> 
+                <RenderComments comments={props.comments} 
+                                addComment={props.addComment}
+                                campsiteId={props.campsite.id}/> 
             </div> 
         </div>    
         )} 
